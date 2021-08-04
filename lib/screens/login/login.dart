51e8aa906cbdb469bac650b9e565d6a0ede63c62
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-04 15:23:17 +0300

    add input field password

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -7,55 +7,61 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     double _width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(24),
-        child: ListView(
-          children: [
-            SizedBox(height: _width / 8,),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 12),
-                  child: Image.asset("name"),
-                ),
-                Expanded(child: Container()),
-            ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: const [
-                Text('Login',
-                style: TextStyle(
-                  fontSize: 30, fontWeight: FontWeight.bold)),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              children:  const [
-                Text('Welcome back!.',
-                style: TextStyle(
-                  fontSize: 10, fontWeight: FontWeight.bold
-                ),), 
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            TextField(
-             decoration: InputDecoration(
-               labelText: 'Email',
-               hintText: 'abc@domain.com',
-               border: OutlineInputBorder(
-                 borderRadius: BorderRadius.circular(20))),
-                 ),
-                 
-          ]
+        body: Container(
+      padding: const EdgeInsets.all(24),
+      child: ListView(children: [
+        SizedBox(
+          height: _width / 8,
         ),
-      )
-    );
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 12),
+              child: Image.asset("name"),
+            ),
+            Expanded(child: Container()),
+          ],
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Row(
+          children: const [
+            Text('Login',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: const [
+            Text(
+              'Welcome back!.',
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        TextField(
+          decoration: InputDecoration(
+              labelText: 'Email',
+              hintText: 'abc@domain.com',
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+        ),
+        const SizedBox(height: 15),
+        TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Password',
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))
+          ),
+        ),
+        const SizedBox(height: 15,),
+      ]),
+    ));
   }
 }
