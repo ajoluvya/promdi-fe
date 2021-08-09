702c0023d4773310749667b00c117135c3c86680
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-09 17:10:19 +0300

    add a constant color outLineBorder

--- lib/helpers/style.dart
+++ lib/helpers/style.dart
@@ -4,5 +4,6 @@ Color light = const Color(0xFFF7F8FC);
 Color lightGrey = const Color(0xFFA4A6B3);
 Color dark = const Color(0xFF363740);
 Color active = const Color(0xFF3C19C0);
-Color lightBlue = const Color(0xFF01579B);
+Color lightBlue = const Color(0xFF246794);
+Color outLineBorder = const Color(0xFFFE0000);
 
