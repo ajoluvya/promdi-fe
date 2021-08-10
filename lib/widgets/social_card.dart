c46edb3c668668b9b00de6330e3aebd6c5150e83
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-10 10:45:23 +0300

    create a social class card

--- lib/widgets/social_card.dart
+++ lib/widgets/social_card.dart
@@ -2,18 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SocialCard extends StatelessWidget {
-  final String icon;
-  final Function press;
+  final String icons;
+  final VoidCallback press;
   const SocialCard({
     Key? key,
-    required this.icon,
+    required this.icons,
     required this.press,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10,),
+        padding: const EdgeInsets.all(8.0),
+        height: 50,
+        width: 50,
+        decoration: const BoxDecoration(
+          color: Colors.blue,
+          shape: BoxShape.rectangle,
+        ),
+
+      child: SvgPicture.asset(icons),
+
+    )
     );
   }
 }
