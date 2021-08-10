c8af72047feb171588ecadf82d9c04fbb7604f6a
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-10 10:08:12 +0300

    create a local variable icon and press

--- lib/widgets/social_card.dart
+++ lib/widgets/social_card.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
-
 class SocialCard extends StatelessWidget {
-  const SocialCard({Key? key}) : super(key: key);
+  final String icon;
+  final Function press;
+  const SocialCard({
+    Key? key,
+    required this.icon,
+    required this.press,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
