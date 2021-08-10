741d8ae36c9c7656b059d95f27451601c8f66186
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-10 10:00:13 +0300

    create a stateless widget

--- lib/widgets/social_card.dart
+++ lib/widgets/social_card.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+
+
+class SocialCard extends StatelessWidget {
+  const SocialCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
