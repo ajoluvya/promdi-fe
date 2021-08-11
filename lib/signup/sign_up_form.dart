634739c70662a7cbf1d89b06efd0a58185fa9371
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-11 15:26:39 +0300

    add a form input text field

--- lib/signup/sign_up_form.dart
+++ lib/signup/sign_up_form.dart
@@ -3,22 +3,53 @@ import 'package:promdi_fe/widgets/custom_button.dart';
 import 'package:promdi_fe/widgets/custom_text.dart';
 import 'package:promdi_fe/helpers/style.dart';
 
-
 import '../size_config.dart';
 
-
 class SignUpForm extends StatefulWidget {
- const  SignUpForm({Key? key}) : super(key: key);
+  const SignUpForm({Key? key}) : super(key: key);
 
   @override
   _SignUpFormState createState() => _SignUpFormState();
 }
 
 class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: null,
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          SizedBox(height: getProportionateScreenHeight(30)),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'First Name',
+              hintText: 'Enter your First Name',
+              labelStyle: TextStyle(color: outLineBorder),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: outLineBorder),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            ),
+          ),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Last Name',
+              hintText: 'Enter your First Name',
+              labelStyle: TextStyle(color: outLineBorder),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: outLineBorder),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
