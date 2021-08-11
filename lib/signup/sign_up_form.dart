e075be39db4eb852fb4b99e462fc365808165c38
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-11 14:51:29 +0300

    add a statfull widget for the Form class

--- lib/signup/sign_up_form.dart
+++ lib/signup/sign_up_form.dart
@@ -5,3 +5,20 @@ import 'package:promdi_fe/helpers/style.dart';
 
 
 import '../size_config.dart';
+
+
+class SignUpForm extends StatefulWidget {
+ const  SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
