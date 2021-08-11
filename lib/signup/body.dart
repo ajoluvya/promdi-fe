45f7cd2263cb835b33b7664b5e682dc216e25ce8
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-11 11:40:45 +0300

    create a stateless widget

--- lib/signup/body.dart
+++ lib/signup/body.dart
@@ -4,4 +4,13 @@ import 'package:promdi_fe/helpers/style.dart';
 import 'package:promdi_fe/size_config.dart';
 
 
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
