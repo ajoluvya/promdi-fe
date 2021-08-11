77e3e073a66f0892b1e7d556ddbe8393c839e25f
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-11 11:38:48 +0300

    import all the dependencies

---
+++ lib/signup/body.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:promdi_fe/widgets/social_card.dart';
+import 'package:promdi_fe/helpers/style.dart';
+import 'package:promdi_fe/size_config.dart';
+
+
+
