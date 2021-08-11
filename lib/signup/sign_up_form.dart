925469933ed94abafbbd3861ca795cada756acc9
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-11 14:47:59 +0300

    import all the dependencies

---
+++ lib/signup/sign_up_form.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:promdi_fe/widgets/custom_button.dart';
+import 'package:promdi_fe/widgets/custom_text.dart';
+import 'package:promdi_fe/helpers/style.dart';
+
+
+import '../size_config.dart';
